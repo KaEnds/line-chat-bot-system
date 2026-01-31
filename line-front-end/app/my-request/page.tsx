65090fac550549ad69289af7f43392bff1704872
chatchanan <kenza.ke15@gmail.com>
2026-01-31 21:45:16 +0700

    Add authentication to my-request page and others-request page

--- line-front-end/app/my-request/page.tsx
+++ line-front-end/app/my-request/page.tsx
@@ -1,14 +1,31 @@
 'use client'
 
 import { useEffect } from "react"
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 function MyRequestContent() {
 
+   const router = useRouter();
+   const { data: session, status } = useSession();
+   console.log('Session Data in MyRequestContent:', session);
+   console.log('Session Status in MyRequestContent:', status);
+
   useEffect(() => {
+    if (status === "loading") {
+      return;
+    }
+    if (status === "unauthenticated") {
+      router.push("/login");
+      return;
+    }
+
+    console.log('Fetching my requests for studentId:', session?.user?.studentId);
+
     fetch('/api/get-my-request', { 
         method: 'POST', 
         headers: { 'Content-Type': 'application/json' }, 
-        body: JSON.stringify({ requesterID: '65070501018' }) 
+        body: JSON.stringify({ requesterID: session?.user?.studentId }) 
     }).then(response => response.json())
     .then(data => {
         console.log('My Requests Data:', data);
@@ -17,7 +34,7 @@ function MyRequestContent() {
         console.error('Error fetching my requests:', error);
       });
 
-  }, [])
+  }, [status, router]);
 
   return (
     <div>MyRequestContent</div>
---
+++ line-front-end/app/others-request/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { useEffect } from "react"
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+function OthersRequestContent() {
+
+    const router = useRouter();
+    const { data: session, status } = useSession();
+
+    useEffect(() => {
+      if (status === "loading") {
+        return;
+      }
+        if (status === "unauthenticated") { 
+        router.push("/login");
+        return;
+      }
+
+      fetch('/api/get-others-request', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      }).then(response => response.json())
+      .then(data => {
+        console.log('Others Requests Data:', data);
+      })
+    
+    }, [status, router]);
+
+  return (
+    <div>OthersRequestContent</div>
+  )
+}
+export default OthersRequestContent
